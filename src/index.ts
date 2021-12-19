5a156b208aeb92c52f8c45e054160c4545e5c255
BreizhReloaded <breizhreloaded@outlook.com>
2021-12-19 23:10:02 +0100

    Init package

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { PiniaPluginContext } from 'pinia';
+import { createStorage, CreateStorageOptions } from 'unstorage';
+
+export const createStoragePlugin = ({ driver }: CreateStorageOptions) => {
+  const storage = createStorage({ driver });
+
+  return ({ store }: PiniaPluginContext) => {
+    storage.getItem(store.$id).then((state) => {
+      Object.assign(store, state);
+    });
+
+    store.$subscribe(() => {
+      storage.setItem(store.$id, JSON.stringify(store.$state));
+    });
+  }
+};
