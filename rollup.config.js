a9d68d9ba79777e9756e20f5e25d2bda6e9dae35
BreizhReloaded <breizhreloaded@outlook.com>
2021-12-22 16:46:03 +0100

    Set up rollup build

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+import p from './package.json';
+
+const name = p.main.replace(/\.cjs$/, '');
+
+const bundle = config => ({
+  ...config,
+  input: 'src/index.ts',
+  external: id => !/^[./]/.test(id),
+});
+
+export default [
+  bundle({
+    plugins: [esbuild()],
+    output: [
+      {
+        file: `${name}.cjs`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true,
+      },
+    ],
+  }),
+  bundle({
+    plugins: [dts()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es',
+    },
+  }),
+];
