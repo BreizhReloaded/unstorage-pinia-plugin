d390679a260e76a38d08a8e5d7606abc415b3f86
BreizhReloaded <breizhreloaded@outlook.com>
2022-03-16 20:17:02 +0100

    Support key filter

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
 import { PiniaPluginContext } from 'pinia';
 import { createStorage, Driver, Storage } from 'unstorage';
 
-const storages: Record<string, Storage> = {};
-
 export interface CreateStoragePluginOptions {
   driver?: Driver
 }
+
+export interface DefineStoreStorageOptions {
+  driver: Driver,
+  filter?: Array<string>
+}
+
+interface StoreStorage {
+  storage: Storage,
+  filter?: Array<string>
+}
+
+const storages: Record<string, StoreStorage> = {};
 
 export const createStoragePlugin = ({ driver }: CreateStoragePluginOptions = {}) => {
   const storage = (driver) ? createStorage({ driver }) : undefined;
 
   return ({ store }: PiniaPluginContext) => {
     if(storages[store.$id]) {
-      storages[store.$id].getItem(store.$id).then((state) => {
+      storages[store.$id].storage.getItem(store.$id).then((state) => {
         Object.assign(store, state);
       });
 
       store.$subscribe(() => {
-        console.log(storages);
-        storages[store.$id].setItem(store.$id, JSON.stringify(store.$state));
+        storages[store.$id].storage.setItem(
+          store.$id,
+          JSON.stringify(Object.fromEntries(
+            Object.entries(store.$state).filter(([key]) => (storages[store.$id].filter?.indexOf(key) ?? 0) > -1)
+          ))
+        );
       });
     }
     else if(storage) {
@@ -33,10 +47,6 @@ export const createStoragePlugin = ({ driver }: CreateStoragePluginOptions = {})
   };
 };
 
-export interface DefineStoreStorageOptions {
-  driver: Driver
-}
-
-export const defineStoreStorage = <Id extends string>(id: Id, { driver }: DefineStoreStorageOptions) => {
-  storages[id] = createStorage({ driver });
+export const defineStoreStorage = <Id extends string>(id: Id, { driver, filter }: DefineStoreStorageOptions) => {
+  storages[id] = { storage: createStorage({ driver }), filter };
 };
